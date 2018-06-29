9b97fdd6221c305bf1d299bab91a42dbe377c432
Ernest <iattempt.net@gmail.com>
2018-06-29 22:49:42 +0800

    Remove unused code

--- test/array/test_n_sum.py
+++ test/array/test_n_sum.py
@@ -25,8 +25,3 @@ class TestNSum(unittest.TestCase):
                                sum_closure=lambda a, b: [a[0] + b[1], a[1] + b[0]],  # noqa: E501
                                compare_closure=lambda a, b: -1 if a[0] < b else 1 if a[0] > b else 0),  # noqa: E501
                          [[[-9, 5], [8, 4]]])  # noqa: E501
-
-
-if __name__ == '__main__':
-
-    unittest.main()
