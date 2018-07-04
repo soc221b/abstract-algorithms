2ed86ff1c421da98ed49ea0bad893b5045e85891
Ernest <iattempt.net@gmail.com>
2018-07-04 21:41:02 +0800

    Remove non-implemnet code

--- lib/sort/__init__.py
+++ lib/sort/__init__.py
@@ -1,6 +1,3 @@
 from lib.sort.bubble_sort.bubble_sort import bubble_sort
 from lib.sort.insertion_sort.insertion_sort import insertion_sort
 from lib.sort.selection_sort.selection_sort import selection_sort
-from lib.sort.quick_sort.quick_sort import quick_sort
-from lib.sort.merge_sort.merge_sort import merge_sort
-from lib.sort.merge_sort.merge_sort_in_place import merge_sort_in_place
