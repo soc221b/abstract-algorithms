26cfb5fd52936e8a1a54d8ea63fd038b9b7ca56f
Ernest <iattempt.net@gmail.com>
2018-10-23 17:11:21 +0800

    Update sorted_singly_linked_list.py

--- lib/linked_list/sorted_singly_linked_list.py
+++ lib/linked_list/sorted_singly_linked_list.py
@@ -1,8 +1,7 @@
 from lib.linked_list import SinglyListNode as ListNode
-from lib.linked_list import SinglyLinkedList
 
 
-class SortedSinglyLinkedList(SinglyLinkedList):
+class SortedSinglyLinkedList():
 
     # O(n)
     def insert(self, x):
@@ -37,3 +36,52 @@ class SortedSinglyLinkedList(SinglyLinkedList):
             raise KeyError
         else:
             return self.__tail
+
+    def __init__(self):
+        self.__head = ListNode(0)
+        self.__tail = self.__head
+        self.__len = 0
+
+    # O(n)
+    def search(self, x):
+        curr = self.__head.next
+        while curr is not None and curr.var != x:
+            curr = self.successor(curr)
+        return curr
+
+    # O(n)
+    def delete(self, node):
+        prev = self.__prev(node)
+        prev.next = node.next
+        self.__len -= 1
+
+    # O(1)
+    def successor(self, node):
+        # no verification for node whether it in this list or not.
+        return node.next
+
+    # O(n)
+    def predecessor(self, node):
+        prev = self.__prev(node)
+        if prev is self.__head:
+            return None
+        else:
+            return prev
+
+    # O(n)
+    def __prev(self, node):
+        prev = self.__head
+        while prev.next is not None and prev.next != node:
+            prev = prev.next
+        if prev.next is None:
+            raise IndexError
+        else:
+            return prev
+
+    # O(1)
+    def __len__(self):
+        return self.__len
+
+    # O(1)
+    def is_empty(self):
+        return self.__len == 0
