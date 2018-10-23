5a2c1f7cf60132f77366942e0d3b8eb0dd07333b
Ernest <iattempt.net@gmail.com>
2018-10-23 11:44:41 +0800

    Update singly_linked_list.py

--- lib/linked_list/singly_linked_list.py
+++ lib/linked_list/singly_linked_list.py
@@ -12,39 +12,42 @@ class SinglyLinkedList():
     # O(1)
     def insert(self, x):
         new_node = ListNode(x)
-        self.__tail.next = new_elem
-        self.__tail = new_elem
+        self.__tail.next = new_node
+        self.__tail = new_node
         self.__len += 1
         return new_node
 
     # O(n)
     def delete(self, node):
-        predecessor = self.predecessor(node)
-        if predecessor is None:  # the node is first node
-            self.__head = node.next
-        else:
-            predecessor.next = node.next
+        prev = self.__prev(node)
+        prev.next = node.next
         self.__len -= 1
 
     # O(1)
     def successor(self, node):
+        # no verification for node whether it in this list or not.
         return node.next
 
     # O(n)
     def predecessor(self, node):
-        curr = self.__head
-        while curr is not None and curr.next != node:
-            curr = curr.next
-        if curr is not None:
-            if curr is self.__head:
-                return None  # if the node is first node
-            else:
-                return curr
+        prev = self.__prev(node)
+        if prev is self.__head:
+            return None
         else:
-            raise KeyError
+            return prev
+
+    # O(n)
+    def __prev(self, node):
+        prev = self.__head
+        while prev.next is not None and prev.next != node:
+            prev = prev.next
+        if prev.next is None:
+            raise IndexError
+        else:
+            return prev
 
     # O(1)
-    def size(self):
+    def __len__(self):
         return self.__len
 
     # O(1)
@@ -53,23 +56,25 @@ class SinglyLinkedList():
 
     # O(n)
     def minimumn(self):
-        return self.__minmax()
+        return self.__minmax(is_max=True)
 
     # O(n)
     def maximum(self):
-        return self.__minmax(False)
+        return self.__minmax(is_min=True)
 
     # O(n)
-    def __minmax(self, is_min=True):
+    def __minmax(self, *, is_min=False, is_max=False):
         if self.is_empty():
             raise KeyError
         else:
             curr = self.__head.next
-            maximum = curr
+            minmax = curr
             while curr.next is not None:
                 curr = curr.next
                 if is_min:
-                    minimum = minimum if minimum.var < curr.var else curr
+                    minmax = minmax if minmax.var < curr.var else curr
+                elif is_max:
+                    minmax = minmax if minmax.var > curr.var else curr
                 else:
-                    maximum = maximum if maximum.var > curr.var else curr
-            return maximum
+                    raise RuntimeError
+            return minmax
