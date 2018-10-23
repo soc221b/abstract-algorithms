6b72e7fdf1c6d46d36022db281569f2511081d27
Ernest <iattempt.net@gmail.com>
2018-10-23 16:26:19 +0800

    Update test_linked_list.py

--- test/linked_list/test_linked_list.py
+++ test/linked_list/test_linked_list.py
@@ -108,7 +108,7 @@ class TestLinkedList(unittest.TestCase):
             for _ in range(0, 100):
                 self.assertEqual(len(ns) == 0, ll.is_empty())
                 if random > 0.6:
-                    r = rand
+                    r = random()
                     ll.insert(r)
                     ns.append(r)
                 if random > 0.4 and len(ns) != 0:
