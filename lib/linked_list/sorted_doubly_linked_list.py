9eb0243d2b720b7e486dbfb82a138a7abc278ccc
Ernest <iattempt.net@gmail.com>
2018-10-23 17:14:03 +0800

    Update sorted_doubly_linked_list.py

--- lib/linked_list/sorted_doubly_linked_list.py
+++ lib/linked_list/sorted_doubly_linked_list.py
@@ -45,3 +45,42 @@ class SortedDoublyLinkedList(DoublyLinkedList):
             raise KeyError
         else:
             return self.__tail
+
+    # O(1)
+    def delete(self, node):
+        predecessor = self.predecessor(node)
+        successor = self.successor(node)
+        if self.size() == 1:
+            self.__head.next = None
+            self.__tail = self.__head
+        elif successor is None:  # the node is last node
+            self.__tail = predecessor
+            predecessor.next = None
+        else:
+            predecessor.next = successor
+            successor.prev = predecessor
+        self.__len -= 1
+
+    # O(1)
+    def predecessor(self, node):
+        return node.prev
+
+    # O(n)
+    def search(self, x):
+        curr = self.__head.next
+        while curr is not None and curr.var != x:
+            curr = self.successor(curr)
+        return curr
+
+    # O(1)
+    def successor(self, node):
+        # no verification for node whether it in this list or not.
+        return node.next
+
+    # O(1)
+    def __len__(self):
+        return self.__len
+
+    # O(1)
+    def is_empty(self):
+        return self.__len == 0
