8aa6ac4df570db724541e1c5ea8300cf63f58153
Ernest <iattempt.net@gmail.com>
2018-10-24 08:12:15 +0800

    Update test_linked_list.py

--- test/linked_list/test_linked_list.py
+++ test/linked_list/test_linked_list.py
@@ -8,131 +8,130 @@ from lib.linked_list import (
     SortedDoublyLinkedList
 )
 
-sorted_linked_lists = [
-    SortedSinglyLinkedList,
-    SortedDoublyLinkedList
-]
-
-unsorted_linked_lists = [
-    SinglyLinkedList,
-    DoublyLinkedList
-]
-
-linked_lists = sorted_linked_lists + unsorted_linked_lists
-
 
 class TestLinkedList(unittest.TestCase):
 
-    def test_insert(self):
-        for linked_list in linked_lists:
-            ll = linked_list()
-            i = 0
-            for r in range(0, 1000):
-                i += 1
-                r = randint(0, 300)
-                node = ll.insert(r)
-                self.assertEqual(node.var, r)
-                self.assertEqual(len(ll), i)
+    def test_signly_linked_list(self):
+        self.__test_unsorted(SinglyLinkedList())
 
-    def test_delete(self):
-        for linked_list in linked_lists:
-            ll = linked_list()
-            for r in range(0, 1000):
-                r = randint(0, 300)
-                node = ll.insert(r)
-                try:
-                    ll.delete(node)
-                except Exception:
-                    self.fail()
+    def test_sorted_signly_linked_list(self):
+        self.__test_sorted(SortedSinglyLinkedList())
 
-    def test_predecessor_for_unsorted(self):
-        for linked_list in linked_lists:
-            ll = linked_list()
-            random_ns = [random() for _ in range(0, 300)]
+    def test_doubly_linked_list(self):
+        self.__test_unsorted(DoublyLinkedList())
+
+    def test_sorted_doubly_linked_list(self):
+        self.__test_sorted(SortedDoublyLinkedList())
+
+    def __test_unsorted(self, linked_list):
+        self.__test_predecessor_for_unsorted(linked_list())
+        self.__test_successor_for_unsorted(linked_list())
+        self.__test_general(linked_list())
+
+    def __test_sorted(self, ll):
+        self.__test_predecessor_for_sorted(linked_list())
+        self.__test_successor_for_sorted(linked_list())
+        self.__test_general(linked_list())
+
+    def __test_general(self, ll):
+        self.__test_insert(linked_list())
+        self.__test_delete(linked_list())
+        self.__test_is_empty(linked_list())
+        self.__test_minimum(linked_list())
+        self.__test_maximum(linked_list())
+
+    def __test_insert(self, ll):
+        i = 0
+        for r in range(0, 1000):
+            i += 1
+            r = randint(0, 300)
+            node = ll.insert(r)
+            self.assertEqual(node.var, r)
+            self.assertEqual(len(ll), i)
+
+    def __test_delete(self, ll):
+        for r in range(0, 1000):
+            r = randint(0, 300)
+            node = ll.insert(r)
+            try:
+                ll.delete(node)
+            except Exception:
+                self.fail()
+
+    def __test_predecessor_for_unsorted(self, ll):
+        random_ns = [random() for _ in range(0, 300)]
+        for n in random_ns:
+            ll.insert(n)
+
+        prev = ll.search(random_ns[0])
+        self.assertEqual(ll.predecessor(prev), None)
+        for n in random_ns[1:]:
+            curr = ll.search(n)
+            self.assertEqual(ll.predecessor(curr), prev)
+            prev = curr
+
+    def __test_predecessor_for_sorted(self, ll):
+        ns = [n for n in range(0, 300)]
+        random_ns = ns[:]
+        shuffle(random_ns)
+        for n in random_ns:
+            ll.insert(n)
+
+        self.assertEqual(ll.predecessor(ll.search(ns[0])), None)
+        for index in range(0, len(ns) - 1):
+            prev = ll.search(ns[index])
+            curr = ll.search(ns[index + 1])
+            self.assertEqual(ll.predecessor(curr), prev)
+
+    def __test_successor_for_unsorted(self, ll):
+        random_ns = [random() for _ in range(0, 300)]
+        for n in random_ns:
+            ll.insert(n)
+
+        next = ll.search(random_ns[-1])
+        self.assertEqual(ll.successor(next), None)
+        for n in random_ns[::-1][1:]:
+            curr = ll.search(n)
+            self.assertEqual(ll.successor(curr), next)
+            next = curr
+
+    def __test_successor_for_sorted(self, ll):
+        ns = [n for n in range(0, 300)]
+        random_ns = ns[:]
+        shuffle(random_ns)
+        for n in random_ns:
+            ll.insert(n)
+
+        self.assertEqual(ll.successor(ll.search(ns[-1])), None)
+        for index in range(0, len(ns) - 1):
+            curr = ll.search(ns[index])
+            next = ll.search(ns[index + 1])
+            self.assertEqual(ll.successor(curr), next)
+
+    def __test_is_empty(self, ll):
+        ns = []
+        for _ in range(0, 100):
+            self.assertEqual(len(ns) == 0, ll.is_empty())
+            if random() > 0.6:
+                r = random()
+                ll.insert(r)
+                ns.append(r)
+            if random() > 0.4 and len(ns) != 0:
+                ll.delete(ll.search(ns[0]))
+                del ns[0]
+
+    def __test_minimum(self, ll):
+        for _ in range(0, 100):
+            random_ns = [random() for _ in range(0, 20)]
+            sorted_ns = sorted(random_ns)
             for n in random_ns:
                 ll.insert(n)
+            self.assertEqual(ll.minimum().var, sorted_ns[0])
 
-            prev = ll.search(random_ns[0])
-            self.assertEqual(ll.predecessor(prev), None)
-            for n in random_ns[1:]:
-                curr = ll.search(n)
-                self.assertEqual(ll.predecessor(curr), prev)
-                prev = curr
-
-    def test_predecessor_for_sorted(self):
-        for linked_list in linked_lists:
-            ll = linked_list()
-            ns = [n for n in range(0, 300)]
-            random_ns = ns[:]
-            shuffle(random_ns)
+    def __test_maximum(self, ll):
+        for _ in range(0, 100):
+            random_ns = [random() for _ in range(0, 20)]
+            sorted_ns = sorted(random_ns)
             for n in random_ns:
                 ll.insert(n)
-
-            self.assertEqual(ll.predecessor(ll.search(ns[0])), None)
-            for index in range(0, len(ns) - 1):
-                prev = ll.search(ns[index])
-                curr = ll.search(ns[index + 1])
-                self.assertEqual(ll.predecessor(curr), prev)
-
-    def test_successor_for_unsorted(self):
-        for linked_list in unsorted_linked_lists:
-            ll = linked_list()
-            random_ns = [random() for _ in range(0, 300)]
-            for n in random_ns:
-                ll.insert(n)
-
-            next = ll.search(random_ns[-1])
-            self.assertEqual(ll.successor(next), None)
-            for n in random_ns[::-1][1:]:
-                curr = ll.search(n)
-                self.assertEqual(ll.successor(curr), next)
-                next = curr
-
-    def test_successor_for_sorted(self):
-        for linked_list in sorted_linked_lists:
-            ll = linked_list()
-            ns = [n for n in range(0, 300)]
-            random_ns = ns[:]
-            shuffle(random_ns)
-            for n in random_ns:
-                ll.insert(n)
-
-            self.assertEqual(ll.successor(ll.search(ns[-1])), None)
-            for index in range(0, len(ns) - 1):
-                curr = ll.search(ns[index])
-                next = ll.search(ns[index + 1])
-                self.assertEqual(ll.successor(curr), next)
-
-    def test_is_empty(self):
-        for linked_list in linked_lists:
-            ll = linked_list()
-            ns = []
-            for _ in range(0, 100):
-                self.assertEqual(len(ns) == 0, ll.is_empty())
-                if random() > 0.6:
-                    r = random()
-                    ll.insert(r)
-                    ns.append(r)
-                if random() > 0.4 and len(ns) != 0:
-                    ll.delete(ll.search(ns[0]))
-                    del ns[0]
-
-    def test_minimumn(self):
-        for linked_list in linked_lists:
-            ll = linked_list()
-            for _ in range(0, 100):
-                random_ns = [random() for _ in range(0, 20)]
-                sorted_ns = sorted(random_ns)
-                for n in random_ns:
-                    ll.insert(n)
-                self.assertEqual(ll.minimum().var, sorted_ns[0])
-
-    def test_maximum(self):
-        for linked_list in linked_lists:
-            ll = linked_list()
-            for _ in range(0, 100):
-                random_ns = [random() for _ in range(0, 20)]
-                sorted_ns = sorted(random_ns)
-                for n in random_ns:
-                    ll.insert(n)
-                self.assertEqual(ll.maximum().var, sorted_ns[-1])
+            self.assertEqual(ll.maximum().var, sorted_ns[-1])
